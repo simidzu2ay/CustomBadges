515a81e2adcdbdc8d9d776e71066fcd9fbc28ae1
simidzu2ay <simidzu2ay@protonmail.com>
2024-01-14 20:48:42 +0100

    Auto update

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@
  */
 
 import { addBadge, BadgePosition, ProfileBadge } from "@api/Badges";
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
 
 const fetchBadges = async (): Promise<Record<string, { name: string, image: string; }[]>> => {
     const result = await fetch("https://raw.githubusercontent.com/simidzu2ay/storage/main/custom-badges.json");
@@ -30,9 +31,31 @@ const applyBadges = (badges: Awaited<ReturnType<typeof fetchBadges>>) => {
     }
 };
 
+
+const settings = definePluginSettings({
+    update: {
+        description: "Does the plugin need to update badges in the background",
+        type: OptionType.BOOLEAN,
+        default: true,
+        restartNeeded: true
+    },
+    howOften: {
+        description: "How often [In seconds]. 0 will be ignored",
+        type: OptionType.NUMBER,
+        default: 60 * 60,
+        restartNeeded: true
+    }
+});
+
+const update = async () => {
+    const badges = await fetchBadges();
+    applyBadges(badges);
+};
+
 export default definePlugin({
     name: "CustomBadges",
     description: "Allow to see custom badges",
+    settings,
     authors: [
         {
             id: 279266228151779329n,
@@ -40,8 +63,11 @@ export default definePlugin({
         }
     ],
     async start() {
-        fetchBadges().then(badges => {
-            return applyBadges(badges);
-        });
+        await update();
+        if (settings.store.update.valueOf() && settings.store.howOften.valueOf() > 0) {
+            setTimeout(async () => {
+                await update();
+            }, settings.store.howOften.valueOf() * 1000);
+        }
     }
 });
